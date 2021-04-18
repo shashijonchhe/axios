115a72a245ad37c2e1fbb5b8b40a54751bf1595c
Shashidhar Jonchhe <shashi@Shashidhars-MacBook-Air.local>
2021-04-17 18:13:15 -0700

    axios call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import * as React from "react";
 import './App.css';
+import {useEffect,useState} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App(){
+    const [counter,setCounter] = useState(0);
+    const [randomData,setRandomData] = useState('');
+
+    useEffect(()=>{
+        fetchRandomData().then(el=>{
+            setRandomData(el)
+        })
+    },[])
+
+    const fetchRandomData=()=>{
+        return axios.get('https://randomuser.me/api')
+        .then((data)=>{
+            return JSON.stringify(data,null, 2)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+    
+
+    return(<div className="App">
+        <h1>{counter}</h1>
+        <button onClick={()=>{
+            setCounter(counter+1);
+            console.log('foo')
+        }}>Counter</button>
+{/* 
+        <button onClick={()=>{
+            fetchRandomData()
+        }}>Get Random Data</button> */} 
+
+        <p>{randomData}</p>
+       
+    </div>)
 }
 
-export default App;
+
+

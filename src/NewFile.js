22dcc4b05608cfe0f17d50804dd31ee1fe1da7f2
Shashidhar Jonchhe <shashi@Shashidhars-MacBook-Air.local>
2021-05-13 10:15:54 -0700

    initial commit

---
+++ src/NewFile.js
@@ -0,0 +1,47 @@
+import * as React from "react";
+import './App.css';
+import {useEffect,useState} from 'react';
+import axios from 'axios';
+
+
+
+export default function App(){
+    const [counter,setCounter] = useState(0);
+    const [randomData,setRandomData] = useState('');
+
+    useEffect(()=>{
+        fetchRandomData().then(el=>{
+            setRandomData(el)
+        })
+    },[])
+
+    const fetchRandomData=()=>{
+        return axios.get('https://randomuser.me/api')
+        .then((data)=>{
+            return JSON.stringify(data,null, 2)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    }
+    
+
+    return(<div className="App">
+        <h1> America </h1>
+        <h1>{counter}</h1>
+        <button onClick={()=>{
+            setCounter(counter+1);
+            console.log('foo')
+        }}>America Counter</button>
+{/* 
+        <button onClick={()=>{
+            fetchRandomData()
+        }}>Get Random Data</button> */} 
+
+        <p>{randomData}</p>
+       
+    </div>)
+}
+
+
+
